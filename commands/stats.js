1f57e3fc2c7a3d1c73574bea858c0d2dea0f95c5
Cut0x <71967731+Cut0x@users.noreply.github.com>
2021-01-16 14:23:43 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    const embed_stats = new Discord.RichEmbed()
+    const embed_stats = new Discord.MessageEmbed()
         .setColor('BLUE')
         .addField(`Stats de __${client.user.username}__ :`, '```ini' + '\n' + `
 +--------------+---------------------+
-| Serveurs     | [${client.guilds.size.toLocaleString()}]
+| Serveurs     | [${client.guilds.cache.size.toLocaleString()}]
 +--------------+---------------------+
-| Utilisateurs | [${client.users.size.toLocaleString()}]
+| Utilisateurs | [${client.users.cache.size.toLocaleString()}]
 +--------------+---------------------+
-| Salons       | [${client.channels.size.toLocaleString()}]
+| Salons       | [${client.channels.cache.size.toLocaleString()}]
 +--------------+---------------------+
 `+ '\n' + '```')
     message.channel.send(embed_stats)
