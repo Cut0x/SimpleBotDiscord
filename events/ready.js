5ee4b7b772afaf4e9df7fa61989aac3ad2418e2a
Cut0x <71967731+Cut0x@users.noreply.github.com>
2021-07-06 01:42:31 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log('Je suis prêt !');
+    name: 'ready',
+    once: true,
+    execute(client) {
+        console.log('Je suis prêt !');
 
         client.user.setActivity("la télé", {
-          type: "WATCHING"
+            type: "WATCHING"
         })
-	},
+    },
 };
